6c7a5e1bd417acc1c7b146edb25be8d348a05564
simpangyo <spg9468@naver.com>
2024-04-09 11:23:20 +0900

    update index.js line27 jsconfig.json line5

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ Coded by www.creative-tim.com
 
 import React from "react";
 import * as ReactDOMClient from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "App";
 
 const container = document.getElementById("root");
@@ -24,7 +24,7 @@ const container = document.getElementById("root");
 const root = ReactDOMClient.createRoot(container);
 
 root.render(
-  <HashRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
