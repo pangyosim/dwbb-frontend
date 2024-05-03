674afb422c72beb3e734ec573d5ae3d3796ef811
simpangyo <spg9468@naver.com>
2024-05-03 11:23:51 +0900

    Update Map Marker

--- src/pages/LandingPages/Map/index.js
+++ src/pages/LandingPages/Map/index.js
@@ -9,6 +9,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Loading from "pages/Presentation/components/Loading";
 import loadingimg from "../../../assets/images/Loading.gif";
+import ibk from "../../../assets/images/ibk.png";
 
 function MapPageBasic () {
     const isLogin = localStorage.getItem("token");
@@ -57,7 +58,6 @@ function MapPageBasic () {
             .catch((error) => console.log('map-data-error : ' + error))
         }
     },[loc.lat,loc.lng])
-    console.log(nearbank)
     return (
         <>
             <MKBox position="fixed" top="0rem" width="100%" zIndex="99">
@@ -79,7 +79,7 @@ function MapPageBasic () {
                 />
             </MKBox>
             <MKBox px={1} width="100%" height="100vh" mx="auto" position="relative" zIndex={2}>
-                {loc.lat !== 0 && nearbank.length !== 0 ?
+                {loc.lat !== 0 && nearbank.length !== 0?
                     <MapDiv
                     style={{
                         width: '100%',
@@ -93,7 +93,39 @@ function MapPageBasic () {
                             <Marker position={new navermaps.LatLng(loc.lat, loc.lng)} onClick={handlerMarkerClick} style={{cursor:"pointer"}} />
                             {nearbank.length !== 0 ? nearbank.map((v,idx)=>{
                                 return(
-                                    <Marker key={idx} position={new navermaps.LatLng(parseFloat(v.geox),parseFloat(v.geoy))} onClick={handlerMarkerClick} style={{cursor:"pointer"}} />
+                                    <Marker key={idx} position={new navermaps.LatLng(parseFloat(v.geoy),parseFloat(v.geox))} style={{cursor:"pointer"}} 
+                                        icon={{
+                                            content: 
+                                            `<style> 
+                                            .arr {
+                                                width: 15vh; 
+                                                height: 4.5vh;
+                                                background: white; 
+                                                border-radius: 10vh; 
+                                                border: 2px solid #0675f4; 
+                                                display: flex; 
+                                                transition: 0.1s linear;
+                                                align-items: center;
+                                            } 
+                                            .arr:after{ 
+                                                border-top: 5px solid #0675f4;
+                                                border-left: 4px solid transparent;
+                                                border-right: 4px solid transparent;
+                                                content: "";
+                                                position: absolute;
+                                                top: 41.5px;
+                                                left: 30px;
+                                            }
+                                            .arr:hover{
+                                                transform: scale(1.05);
+                                            }
+                                            </style>
+                                                <div class="arr">
+                                                    <img src=${ibk} style="margin-left: 0.75vh; width: 2.5vh; height: 2.5vh;"/><p style="margin-top: 0.5vh; margin-left: 1vh; font-size: 12px; font-weight: bold;"> IBK기업은행 <br>${v.krnbrm}</p>
+                                                </div>`,
+                                                scaledSize: new navermaps.Size(50, 50),
+                                        }}
+                                    />
                                 )
                             }) : ""}
                         </NaverMap>
